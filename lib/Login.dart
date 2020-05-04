4f31506726ba8602ae109a8855095148895cc20a
Edson Calixto <edsondev@MacBook-Pro-de-Edson.local>
2020-05-04 20:58:40 -0300

    App

---
+++ lib/Login.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:unna/screens/feed_screen.dart';
+import 'Cadastro.dart';
+
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 60),
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    width: 200,
+                    height: 150,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "E-mail",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                ),
+                TextField(
+                  obscureText: true,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Senha",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32))),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 10),
+                  child: RaisedButton(
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      color: Colors.blue[600],
+                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FeedScreen()
+                          )
+                      );
+                      }),
+                ),
+                Center(
+                  child: GestureDetector(
+                    child: Text(
+                        "Cadastre-se!",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red
+                        )
+                    ),
+                    onTap: (){
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Cadastro()
+                          )
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 16),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:unna/Login.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Instagram Feed UI Redesign',
+      debugShowCheckedModeBanner: false,
+      home: Login()
+    );
+  }
+}
